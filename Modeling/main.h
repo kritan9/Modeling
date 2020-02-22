76ef83929b7057dc864a0c1a93bca343032e15c3
kritan <074bct516.kritan@pcampus.edu.np>
2020-02-22 13:51:50 +0545

    minor fixes

---
+++ Modeling/main.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stb_image.h"
+#include "camera.h"
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glad/glad.h> // holds all OpenGL type declarations
+
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "shader.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <map>
+
+
+#define WIDTH 1366
+#define HEIGHT 768
+using namespace std;
+
+#endif
